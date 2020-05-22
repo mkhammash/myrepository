2316d270b195f28234f56f5352ba6365c2276be7
mkhammash <muhannad.khammash@hotmail.com>
2020-05-23 03:36:55 +0400

    Add files via upload

---
+++ S3-Bucket.tf
@@ -0,0 +1,107 @@
+
+
+######################################################
+ # Change bucket from "public-read-write" to "private"
+######################################################
+resource "aws_s3_bucket" "default" {
+  bucket = "${var.name}"
+  acl = "public-read-write"
+
+  logging {
+    target_bucket = "${var.logging_target_bucket}"
+    target_prefix = "logs/${var.name}/"
+  }
+  
+######################################################
+ # Change Versioning from "false" to "true"
+######################################################
+  versioning {
+    enabled = false
+  }
+
+  lifecycle_rule {
+    enabled = "${var.lifecycle_rule_enabled}"
+    prefix  = "${var.lifecycle_rule_prefix}"
+
+    transition {
+      days          = "${var.standard_ia_transition_days}"
+      storage_class = "STANDARD_IA"
+    }
+
+    transition {
+      days          = "${var.glacier_transition_days}"
+      storage_class = "GLACIER"
+    }
+
+    expiration {
+      days = "${var.expiration_days}"
+    }
+
+    noncurrent_version_transition {
+      days          = "${var.glacier_noncurrent_version_transition_days}"
+      storage_class = "GLACIER"
+    }
+
+    noncurrent_version_expiration {
+      days = "${var.noncurrent_version_expiration_days}"
+    }
+  }
+
+  force_destroy = "${var.force_destroy}"
+
+  tags = "${var.tags}"
+}
+
+resource "aws_s3_bucket_policy" "default" {
+  bucket = "${aws_s3_bucket.default.id}"
+  policy = "${data.aws_iam_policy_document.default.json}"
+}
+
+data "aws_iam_policy_document" "default" {
+  statement {
+    sid    = "AWSCloudTrailAclCheck"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+    actions = [
+      "s3:GetBucketAcl",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${var.name}",
+    ]
+  }
+
+  statement {
+    sid    = "AWSCloudTrailWrite"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+    actions = [
+      "s3:PutObject",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${var.name}/*",
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+
+      values = [
+        "bucket-owner-full-control",
+      ]
+    }
+
+    
+  }
+}
---
+++ securitygroups.tf
@@ -0,0 +1,40 @@
+######################################################
+# Web Server Security Group for our Production VPC
+######################################################
+resource "aws_security_group" "WebServer" {
+  vpc_id      = "${aws_vpc.Production-VPC.id}"
+  description = "WebServer SG"
+  name        = "WebServerSG"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+######################################################
+# Change ingress cidr_blocks from "0.0.0.0/0" 
+# to "24.55.20.30/32"
+######################################################
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
